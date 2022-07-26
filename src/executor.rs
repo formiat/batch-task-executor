1061e8936b578067b77d89a267a6a3c0fbe355f6
Alexander Aglerimov <formiat95@gmail.com>
2022-07-26 14:21:29 +0400

    made "fn batch_executor()" non-recursive

--- src/executor.rs
+++ src/executor.rs
@@ -1,41 +1,46 @@
-use futures::future::{BoxFuture, FutureExt};
 use std::fmt::Debug;
 
 const THRESHOLD: usize = 3;
 
 /// TODO: Remove `Clone` requirement for `T`
-pub fn batch_executor<'a, T, U, F>(input: Vec<T>, func: F) -> BoxFuture<'a, Vec<U>>
+pub async fn batch_executor<T, U, F>(input: Vec<T>, func: F) -> Vec<U>
 where
-    T: Send + Clone + Debug + 'a,
-    U: Send + Debug + 'a,
-    F: Send + Copy + Fn(T) -> U + 'a,
+    T: Clone + Debug,
+    U: Debug,
+    F: Fn(T) -> U,
 {
-    async move {
-        if input.len() > THRESHOLD {
-            // TODO: Replace with consuming `chunks`. Remove `v.to_vec()` call
-            let inputs: Vec<Vec<_>> = input.chunks(THRESHOLD).map(|v| v.to_vec()).collect();
-            println!("inputs: {:#?}", inputs);
+    if input.len() > THRESHOLD {
+        // TODO: Replace with consuming `chunks`. Remove `v.to_vec()` call
+        let inputs: Vec<Vec<_>> = input.chunks(THRESHOLD).map(|v| v.to_vec()).collect();
+        println!("inputs: {:#?}", inputs);
 
-            let mut futures = Vec::new();
-            for input in inputs {
-                let future = batch_executor(input, func);
+        let mut futures = Vec::new();
+        for input in inputs {
+            let future = batch_executor_inner(input, &func);
 
-                futures.push(future);
-            }
-            let res = futures::future::join_all(futures).await;
-            println!("res: {:#?}", res);
-
-            res.into_iter().flatten().collect()
-        } else {
-            let mut results = Vec::new();
-            for item in input {
-                let res = func(item);
-
-                results.push(res);
-            }
-
-            results
+            futures.push(future);
         }
+        println!("futures count: {}", futures.len());
+        let res = futures::future::join_all(futures).await;
+        println!("res: {:#?}", res);
+
+        res.into_iter().flatten().collect()
+    } else {
+        batch_executor_inner(input, func).await
     }
-    .boxed()
+}
+
+async fn batch_executor_inner<T, U, F>(input: Vec<T>, func: F) -> Vec<U>
+where
+    F: Fn(T) -> U,
+{
+    let mut results = Vec::new();
+
+    for item in input {
+        let res = func(item);
+
+        results.push(res);
+    }
+
+    results
 }
