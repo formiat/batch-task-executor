ca4c1459133ef59b108bf7c0d267ec4d6de1dca0
Alexander Aglerimov <formiat95@gmail.com>
2022-08-04 13:25:30 +0400

    replaced loops with iterators, added "tokio::spawn" before "join_all"

--- src/executor.rs
+++ src/executor.rs
@@ -5,26 +5,25 @@ const THRESHOLD: usize = 3;
 /// TODO: Remove `Clone` requirement for `T`
 pub async fn batch_executor<T, U, F>(input: Vec<T>, func: F) -> Vec<U>
 where
-    T: Clone + Debug,
-    U: Debug,
-    F: Fn(T) -> U,
+    T: Clone + Debug + Send + 'static,
+    U: Debug + Send + 'static,
+    F: Fn(T) -> U + Copy + Send + 'static,
 {
     if input.len() > THRESHOLD {
         // TODO: Replace with consuming `chunks`. Remove `v.to_vec()` call
         let inputs: Vec<Vec<_>> = input.chunks(THRESHOLD).map(|v| v.to_vec()).collect();
         println!("inputs: {:#?}", inputs);
 
-        let mut futures = Vec::new();
-        for input in inputs {
-            let future = batch_executor_inner(input, &func);
-
-            futures.push(future);
-        }
+        let futures: Vec<_> = inputs
+            .into_iter()
+            .map(|input| tokio::spawn(batch_executor_inner(input, func)))
+            .collect();
         println!("futures count: {}", futures.len());
+
         let res = futures::future::join_all(futures).await;
         println!("res: {:#?}", res);
 
-        res.into_iter().flatten().collect()
+        res.into_iter().flat_map(Result::unwrap).collect()
     } else {
         batch_executor_inner(input, func).await
     }
@@ -34,13 +33,5 @@ async fn batch_executor_inner<T, U, F>(input: Vec<T>, func: F) -> Vec<U>
 where
     F: Fn(T) -> U,
 {
-    let mut results = Vec::new();
-
-    for item in input {
-        let res = func(item);
-
-        results.push(res);
-    }
-
-    results
+    input.into_iter().map(func).collect()
 }
